8d88d03771d1e43eba26cff004a88f68b011133b
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2025-01-01 23:28:33 +0500

    Adding more parameters in a constructor

--- src/app/student.ts
+++ src/app/student.ts
@@ -5,7 +5,9 @@ export class Student {
     constructor(
         public name?: string,
         public age?: number,
-        public email?: string
+        public email?: string,
+        public gender?: string,
+        public country?: string
     ) {
 
     }
